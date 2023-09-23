b526e038a417aabc87c88090c4f5d08b057ed7b8
Felipe Simões <ffelipesimoes@gmail.com>
2023-09-23 10:14:59 -0400

    startup hardhat

---
+++ backend/hardhat.config.js
@@ -0,0 +1,47 @@
+const { HardhatUserConfig } = require('hardhat/config');
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+
+const config = {
+  solidity: {
+    version: '0.8.19',
+  },
+  networks: {
+    // for mainnet
+    'base-mainnet': {
+      url: 'https://mainnet.base.org',
+      accounts: [process.env.WALLET_KEY],
+      gasPrice: 1000000000,
+    },
+    // for testnet
+    'base-goerli': {
+      url: 'https://goerli.base.org',
+      accounts: [process.env.WALLET_KEY],
+      gasPrice: 1000000000,
+    },
+    // for local dev environment
+    'base-local': {
+      url: 'http://localhost:8545',
+      accounts: [process.env.WALLET_KEY],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    apiKey: {
+     "base-goerli": "PLACEHOLDER_STRING"
+    },
+    customChains: [
+      {
+        network: "base-goerli",
+        chainId: 84531,
+        urls: {
+         apiURL: "https://api-goerli.basescan.org/api",
+         browserURL: "https://goerli.basescan.org"
+        }
+      }
+    ]
+  },
+  defaultNetwork: 'hardhat',
+};
+
+module.exports = config;
